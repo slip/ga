2886dd88cda3cc1a08146d957cf7e80b5f2d0ac8
Ian Kennedy <exposure@gmail.com>
2016-12-17 14:14:15 -0500

    added mail chimp to widget and front page. closes #46

--- front-page.php
+++ front-page.php
@@ -36,11 +36,12 @@ get_header(); ?>
 			<div class="hp-newsletter">
 				<div class="lg-container clear">
 					<div class="wrapper clear">
-						<form>
-							<div class="clear">
-								<input type="email" placeholder="you@email.com" /><input type="submit" value="Newsletter" />
-							</div>
-						</form>
+            <form class="clear" action="//GreggAllman.us14.list-manage.com/subscribe/post?u=7f78ff63f40071f909f81904f&amp;id=067dc401ba" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" no>
+        			<input type="email" name="EMAIL" id="mce-EMAIL" placeholder="you@email.com" />
+              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_7f78ff63f40071f909f81904f_067dc401ba" tabindex="-1" value=""></div>
+              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" />
+        		</form>
 						<div class="social-icons">
 							<a href="https://www.youtube.com/user/officialgreggallman" target="_blank" title="YouTube"><?php get_template_part('template-parts/icons/icon', 'youtube.svg'); ?></a>
 							<a href="http://facebook.com/GreggAllman" target="_blank" title="Facebook"><?php get_template_part('template-parts/icons/icon', 'facebook.svg'); ?></a>
