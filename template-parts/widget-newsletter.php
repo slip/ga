8af1a49e61cbbc9e6aa9aaa21558c60956002ee2
Ian Kennedy <exposure@gmail.com>
2016-12-18 01:35:47 -0500

    fixed widget social links.

--- template-parts/widget-newsletter.php
+++ template-parts/widget-newsletter.php
@@ -1,10 +1,10 @@
 <div class="newsletter">
 	<div class="content">
 		<div class="social-icons">
-			<a href="#" target="_blank" title="YouTube"><?php get_template_part('template-parts/icons/icon', 'youtube.svg'); ?></a>
-				<a href="#" target="_blank" title="Facebook"><?php get_template_part('template-parts/icons/icon', 'facebook.svg'); ?></a>
-				<a href="#" target="_blank" title="Instagram"><?php get_template_part('template-parts/icons/icon', 'instagram.svg'); ?></a>
-				<a href="#" target="_blank" title="Twitter"><?php get_template_part('template-parts/icons/icon', 'twitter.svg'); ?></a>
+			<a href="https://www.youtube.com/user/officialgreggallman" target="_blank" title="YouTube"><?php get_template_part('template-parts/icons/icon', 'youtube.svg'); ?></a>
+				<a href="http://facebook.com/GreggAllman" target="_blank" title="Facebook"><?php get_template_part('template-parts/icons/icon', 'facebook.svg'); ?></a>
+				<a href="http://instagram.com/OfficialGreggAllman" target="_blank" title="Instagram"><?php get_template_part('template-parts/icons/icon', 'instagram.svg'); ?></a>
+				<a href="http://twitter.com/GreggAllman" target="_blank" title="Twitter"><?php get_template_part('template-parts/icons/icon', 'twitter.svg'); ?></a>
 		</div>
 		<form class="clear" action="//GreggAllman.us14.list-manage.com/subscribe/post?u=7f78ff63f40071f909f81904f&amp;id=067dc401ba" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" no>
 			<input type="email" name="EMAIL" id="mce-EMAIL" placeholder="you@email.com" />
