6254f7d06baf8fbc58e9a8743edafbac4d8a5ca6
Ian Kennedy <exposure@gmail.com>
2016-12-23 15:33:42 -0500

    revised individual tour date pages. closes #97

--- template-parts/content-ga_tour_dates.php
+++ template-parts/content-ga_tour_dates.php
@@ -13,7 +13,7 @@
 	$date = new DateTime($date);
 	$city = get_field('city');
 	$status = get_field('status');
-	$ticket = get_field('ticket_link'); 
+	$ticket = get_field('ticket_link');
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
@@ -21,34 +21,26 @@
 	<figure class="featured-image">
 		<?php the_post_thumbnail('medium'); ?>
 	</figure>
-	
+
 	<header class="entry-header">
 		<?php
-			the_title( '<h1 class="entry-title">', '</h1>' );	
+			the_title( '<h1 class="entry-title">', '</h1>' );
 		?>
 			<p class="subheading"><?php echo $city; ?></p>
 			<p class="subheading"><?php echo $date->format('F j, Y'); ?></p>
 	</header><!-- .entry-header -->
-	
+
 	<div class="entry-content">
 		<div class="ticket">
-			<?php 
+			<?php
 			if($status == 'onsale'):
 			?>
 				<a href="<?php echo $ticket; ?>" target="_blank"><?php get_template_part('template-parts/icons/icon', 'on-sale.svg'); ?></a>
-			<?php	
+			<?php
 			elseif($status == 'soldout'):
-				get_template_part('template-parts/icons/icon', 'sold-out.svg'); 
+				get_template_part('template-parts/icons/icon', 'sold-out.svg');
 			endif;
 			?>
-		</div>
-		<div class="tour-links">
-			<p>
-			<?php if($status == 'onsale'): ?>
-			<a href="<?php echo $ticket; ?>" target="_blank">Buy Tickets</a><br />
-			<?php endif; ?>
-			<a href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&amp;t=<?php the_title(); ?>" title="Share on Facebook.">Share on Facebook</a><br />
-			<a href="http://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?>" title="Tweet this!">Tweet this</a></p>	
 		</div>
 	<?php
 		the_content( sprintf(
@@ -56,7 +48,7 @@
 			wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'resolutionathens' ), array( 'span' => array( 'class' => array() ) ) ),
 			the_title( '<span class="screen-reader-text">"', '"</span>', false )
 		) );
-		
+
 		wp_link_pages( array(
 			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'resolutionathens' ),
 			'after'  => '</div>',
@@ -78,7 +70,7 @@
 		</div>
 	</a>
 	<?php endif; ?>
-	
-	
+
+
 
 </article><!-- #post-## -->
